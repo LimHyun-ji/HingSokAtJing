5905db43736ae769163053069f2211c025c29e9a
p4r4dox82 <p4r4dox82@snu.ac.kr>
2022-07-17 03:15:37 +0900

    .

--- Assets/Scripts/PlayerHandController.cs
+++ Assets/Scripts/PlayerHandController.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class PlayerHandController : MonoBehaviour
 {
-    float mouseDistance =10;
-    public string controllerName="";
+    float mouseDistance = 10;
+    public string controllerName = "";
     private PlayerHandController otherHand;
     private string otherHandName;
     //private bool isDumbelGripRight;
@@ -16,7 +16,7 @@ public class PlayerHandController : MonoBehaviour
     protected bool isExercise;
     protected bool isOnStartPos;
     protected bool reachEndPoint;
-    
+
     private GameObject dumbbell;
     private GameObject startPoint;
     private GameObject endPoint;//이거는 안보이게 
@@ -27,13 +27,13 @@ public class PlayerHandController : MonoBehaviour
 
     void Start()
     {
-        //GameManager.Instance().addPlayerHandController(gameObject.GetComponent<PlayerHandController>());
-        otherHandName=(controllerName=="L"? "Right":"Left");
-        dumbbell=GameObject.Find("Dumbel"+controllerName);
-        otherHand=GameObject.Find("PlayerHand"+otherHandName).GetComponent<PlayerHandController>();
-        startPoint =GameObject.Find("StartPos"+controllerName);
-        endPoint=GameObject.Find("EndPoint"+controllerName);
-        trackLine=GameObject.Find("TrackLine"+controllerName);
+        GameManager.Instance().addPlayerHandController(gameObject.GetComponent<PlayerHandController>());
+        otherHandName = (controllerName == "L" ? "Right" : "Left");
+        dumbbell = GameObject.Find("Dumbel" + controllerName);
+        otherHand = GameObject.Find("PlayerHand" + otherHandName).GetComponent<PlayerHandController>();
+        startPoint = GameObject.Find("StartPos" + controllerName);
+        endPoint = GameObject.Find("EndPoint" + controllerName);
+        trackLine = GameObject.Find("TrackLine" + controllerName);
         startPoint.SetActive(false);
         endPoint.SetActive(false);
         trackLine.SetActive(false);
@@ -41,43 +41,43 @@ public class PlayerHandController : MonoBehaviour
 
     void Update()
     {
-        if(Input.GetMouseButtonUp(0))
+        if (Input.GetMouseButtonUp(0))
         {
             Debug.Log(isDumbbellGrip);
-            dumbbell.gameObject.transform.parent=null;
-            isDumbbellGrip=false;
+            dumbbell.gameObject.transform.parent = null;
+            isDumbbellGrip = false;
             trackLine.SetActive(false);
             endPoint.SetActive(false);
-        }        
+        }
     }
-    private void OnMouseDrag() 
+    private void OnMouseDrag()
     {
         Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, mouseDistance);
-        Vector3 objPosition= Camera.main.ScreenToWorldPoint(mousePosition);
-        transform.position=objPosition;
+        Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        transform.position = objPosition;
     }
 
     private void OnTriggerEnter(Collider other) //항상 양쪽 다 체크해야 함
     {
-        if(other.gameObject.name.Contains("Dumbel"+controllerName))
+        if (other.gameObject.name.Contains("Dumbel" + controllerName))
         {
-            isDumbbellGrip=true;
+            isDumbbellGrip = true;
             dumbbell.transform.SetParent(gameObject.transform);//이건 나중에 없애줌
 
-            if(otherHand.isDumbbellGrip)
+            if (otherHand.isDumbbellGrip)
             {
-                //GameManager.Instance().setGrabDumbbell(true);
-                isExercise=true;
+                GameManager.Instance().setGrabDumbbell(true);
+                isExercise = true;
                 otherHand.startPoint.SetActive(true);
                 startPoint.SetActive(true);
             }
         }
-        
-        if(other.gameObject.name.Contains("StartPos"+controllerName))
+
+        if (other.gameObject.name.Contains("StartPos" + controllerName))
         {
-            isOnStartPos=true;
+            isOnStartPos = true;
             startPoint.SetActive(false);
-            if(otherHand.isOnStartPos)
+            if (otherHand.isOnStartPos)
             {
                 trackLine.SetActive(true);
                 endPoint.SetActive(true);
@@ -86,40 +86,45 @@ public class PlayerHandController : MonoBehaviour
                 count++;
                 otherHand.count++;
             }
-            // if(count ==0)
-            //     GameManager.Instance().setStartExercise(true);
-            // else    
-            //     GameManager.Instance().addTimesOfExercise(true);
+            if (count == 0)
+                GameManager.Instance().setStartExercise(true);
+            else
+                GameManager.Instance().addTimesOfExercise();
 
-         
+
         }
-        if(other.gameObject.name.Contains("TrackLine"+ controllerName))
+        if (other.gameObject.name.Contains("TrackLine" + controllerName))
         {
             //트랙 경로가 닿은 만큼 줄어들기(Slider처럼)
-            isOutofTrackLine=true;
+            isOutofTrackLine = true;
         }
-        if(other.gameObject.name.Contains("EndPoint"+controllerName))
+        if (other.gameObject.name.Contains("EndPoint" + controllerName))
         {
-            reachEndPoint=true;
+            reachEndPoint = true;
         }
 
     }
     private void OnTriggerExit(Collider other) // 한쪽만 벗어나도 호출
     {
-        if(other.gameObject.name.Contains("Dumbel"+controllerName))//덤벨을 놓으면
+        if (other.gameObject.name.Contains("Dumbel" + controllerName))//덤벨을 놓으면
         {
-            // isDumbbellGrip=false;
-            // trackLine.SetActive(false);
-            // endPoint.SetActive(false);
-            //GameManager.Instance().setGrabDumbbell(false);
+            isDumbbellGrip = false;
+            trackLine.SetActive(false);
+            endPoint.SetActive(false);
+            GameManager.Instance().setGrabDumbbell(false);
         }
-        if(other.gameObject.name.Contains("TrackLine"+ controllerName))
+        if (other.gameObject.name.Contains("TrackLine" + controllerName))
         {
             //트랙 경로를 벗어나면
             //Warning
-            isOutofTrackLine=true;
+            isOutofTrackLine = true;
         }
-        
+
     }
-    
+
+    public void setIsExercise()
+    {
+        isExercise = false;
+    }
+
 }
